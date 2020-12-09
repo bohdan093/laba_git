b60cafc203f1d24defc19f2c4573b162b5997366
bohdan093 <bohdan.inform@gmail.com>
2020-12-09 16:44:16 +0200

    new stance

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("hello");
    Junior junior = new Junior(18, "Bohdan");
         System.out.println(junior);
+        System.out.println(junior);
     }
 }
