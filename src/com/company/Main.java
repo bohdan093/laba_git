e0a07513b94165dc88c212c6f80f59dc3d11400d
bohdan093 <bohdan.inform@gmail.com>
2020-12-09 16:36:04 +0200

    Add new flow

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello");
-   
+   Junior junior = new Junior(18, "Bohdan");
+        System.out.println(junior);
     }
 }
