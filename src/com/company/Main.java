7dcef0380628fff16e666b41ea84cebccf9ce618
bohdan093 <bohdan.inform@gmail.com>
2020-12-09 20:11:32 +0200

    only hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello");
-   Junior junior = new Junior(18, "Bohdan");
-        System.out.println(junior);
-        System.out.println(junior);
+
     }
 }
